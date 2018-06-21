8bf805d2fd5e42ba03f87240720eadb32682a3c5
Alex <36311037+sirenkoJr@users.noreply.github.com>
2018-06-21 13:41:04 +0300

    Revert "create logic of todoAdvancedList: actions, reducers, container, began to create components"
    This reverts commit 8b1fea6076dd45476258a73e867f45b01e33b78a.

--- development/js/reducers/addForm.js
+++
@@ -1,8 +0,0 @@
-export const isFormOpen = (state = false, action) => {
-    switch (action.type) {
-        case 'ADD_FORM_TOGGLE': 
-            return !state
-        default:
-            return state
-    }
-}
--- development/js/reducers/onClickUpdateItem.js
+++
@@ -1,8 +0,0 @@
-export const updateItemId = (state = null, action) => {
-    switch(action.type){
-        case 'ON_CLICK_UPDATE_ITEM': 
-            return action.id
-        default: 
-            return state
-    }
-}
